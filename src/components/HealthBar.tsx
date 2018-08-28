e5b2c3728c56e127fdca1401eec2df42ec25aa92
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2018-08-28 23:43:43 +0200

    Initial commit

---
+++ src/components/HealthBar.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class HealthBar extends React.Component {
+    render() {
+        return <div>HP</div>
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { HealthBar } from './components/HealthBar';
+
+ReactDOM.render(
+    <HealthBar/>,
+    document.getElementById('game-gui'),
+);
