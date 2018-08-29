e7f3b44e8222fc8a72f29b7f92eaf7ecab77aded
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2018-08-29 22:23:20 +0200

    client-server-common structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { HealthBar } from './components/HealthBar';
+import { log } from '../../common/Common';
 
 ReactDOM.render(
     <HealthBar/>,
     document.getElementById('game-gui'),
 );
+
+log('hello');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         ]
     },
 
-    mode: "development",
+    mode: "development"
 
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
