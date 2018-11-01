76a22e5d1c631334b26b376b008aa89b6191b384
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2018-11-01 01:15:39 +0100

    Disable context menu, set selection color, credits overflow fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from './App';
 
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
 ReactDOM.render(
     <App/>,
     document.getElementById('game-gui'),
