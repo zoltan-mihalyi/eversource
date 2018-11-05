ff310ca85407344cbd71db30825c5a4d8c55c878
Zoltan Mihalyi <zoltan.mihalyi@webvalto.hu>
2018-11-05 17:52:43 +0100

    Fix context menu vibration on chrome for android

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from './App';
 
-document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('touchstart', handleTouch);
+document.addEventListener('touchend', handleTouch);
+document.addEventListener('touchcancel', handleTouch);
+
+let touching = false;
+
+function handleTouch(e: TouchEvent) {
+    touching = e.touches.length !== 0;
+}
+
+document.body.addEventListener('contextmenu', (e) => {
+    if (!touching) { // Disable vibration on chrome for android
+        e.preventDefault();
+    }
+});
 
 ReactDOM.render(
     <App/>,
