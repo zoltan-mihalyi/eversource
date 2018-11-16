b470521f63ea93c0f96b2fa652cbb78c245ebfad
Zoltan Mihalyi <zoltan.mihalyi@webvalto.hu>
2018-11-16 18:05:10 +0100

    Cache-Control: no-transform in order to disable proxy png compression

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,9 +14,15 @@ const humanoidPresets = JSON.parse(fs.readFileSync('./data/presets.json', 'utf-8
 const world = new WorldImpl(mapLoader, humanoidPresets, monsterPresets);
 
 const app = express();
-app.use(express.static('../cordova/www'));
+app.use(express.static('../cordova/www', { setHeaders }));
 const httpServer = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const gameServer = new Server(new FakeDao(), world, {
     server: httpServer,
 });
+
+function setHeaders(res: express.Response, path: string) {
+    if (path.endsWith('.png')) {
+        res.header('Cache-Control', 'no-transform, public, max-age=0');
+    }
+}
